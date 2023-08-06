2733c05cc86f0da162e80284e6f4a57d47c9918e
HicaroD <hdanrlley1@gmail.com>
2023-08-06 15:02:58 -0300

    fix(#16): wip

--- lua/alice/theme.lua
+++ lua/alice/theme.lua
@@ -115,7 +115,7 @@ theme.set_style = function()
   highlight("@error", { fg = colors.error_red })
   highlight("@parameter", { fg = colors.orange })
   highlight("@constructor", { fg = colors.yellow })
-  highlight("@constant", { fg = colors.tifanny_blue_aqua })
+  highlight("@constant", { fg = colors.light_blue })
 
   -- COC.nvim
   -- @@@ TODO: change colors of errors, warning and hints
