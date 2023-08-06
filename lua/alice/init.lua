206be00dfae6808bb360cbedb3726e87ab103170
HicaroD <hdanrlley1@gmail.com>
2023-08-06 00:14:37 -0300

    fix(#14): adding `alice.lua` at `colors/` folder

--- lua/alice/init.lua
+++ lua/alice/init.lua
@@ -1,19 +1,15 @@
 local alice = {}
 
-local theme = require("lua.alice.theme")
+local theme = require("alice.theme")
 
 alice.load = function()
   vim.cmd('hi clear')
-  if vim.fn.exists('syntax_on') then
-    vim.cmd('syntax reset')
-  end
+  if vim.fn.exists('syntax_on') then vim.cmd('syntax reset') end
 
   vim.o.termguicolors = true
   vim.g.colors_name = "alice"
 
   theme.set_style()
 end
-
-alice.load()
 
 return alice
